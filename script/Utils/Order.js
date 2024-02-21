d521ad2e0c65a8bbf515de9438031fe2e5406d30
thecode00 <thecode0087@gmail.com>
2024-02-22 05:14:01 +0900

    first commit

---
+++ script/Utils/Order.js
@@ -0,0 +1,12 @@
+class Order {
+    constructor(orderPrice, margin, ticker, orderType) {
+        this.orderPrice = orderPrice
+        this.margin = margin
+        this.ticker = ticker
+        this.orderType = orderType // 0 = Long, 1 = Short
+    }
+
+    sell() {
+        if 
+    }
+}
---
+++ script/app.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("trade-button");
+console.log(btn);
+btn.addEventListener("click", (event) => {
+	event.preventDefault();
+});
