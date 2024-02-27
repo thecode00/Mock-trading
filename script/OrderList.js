0f1add7901d9c48af54f8925a47d9365a52a7a73
thecode00 <thecode0087@gmail.com>
2024-02-27 14:34:19 +0900

    Init

---
+++ script/OrderList.js
@@ -0,0 +1,6 @@
+export class OrderList {
+	lists = [];
+	constructor(type) {
+		this.type = type;
+	}
+}
--- script/app.js
+++ script/app.js
@@ -1,5 +1,31 @@
+import { OrderItem } from "./OrderItem.js";
+import { OrderList } from "./OrderList.js";
 import { BinanceSocket } from "./Utils/BinanceSocket.js";
 
 const socket = new BinanceSocket("btc");
+const changeTickerButton = document.getElementById("change-ticker-button");
 
-socket.changeTicker("eth");
+const orderForm = document.getElementById("order-form");
+
+const changeTickerHandler = () => {
+	const ticker = document.getElementById("change-ticker-input").value;
+	if (ticker.trim().length === 0) {
+		return;
+	}
+
+	socket.changeTicker(ticker.toLocaleLowerCase());
+};
+changeTickerButton.addEventListener("click", changeTickerHandler);
+
+const openOrderList = new OrderList("order");
+const openPositionList = new OrderList("position");
+
+orderForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const targetPrice = orderForm.querySelector("#price-input").value;
+	const margin = orderForm.querySelector("#margin-input").value;
+	console.log(targetPrice, margin);
+
+	openOrderList.lists.push(new OrderItem(targetPrice, margin, "BtC", 1));
+});
