860bc428d2c3826ba823eb0b09f968d302e305e4
thecode00 <thecode0087@gmail.com>
2024-03-08 22:00:37 +0900

    Change structure

--- script/Utils/BinanceSocket.js
+++ script/Utils/BinanceSocket.js
@@ -1,16 +1,33 @@
 export class BinanceSocket {
     constructor(ticker) {
-        this.ws = new WebSocket(`wss://stream.binance.com:9443/ws/btcusdt@trade`);
+        this.ws = new WebSocket(`wss://stream.binance.com:9443/ws`);
         this.ticker = ticker;
         console.log(`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`);
         this.connect();
+        // this.send();
     }
     connect() {
         const p = document.getElementById("ticker-price");
-        this.ws.close();
-        this.ws = new WebSocket(`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`);
-        console.log(`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`);
+        // this.ws.close();
+        // this.ws = new WebSocket(
+        // 	`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
+        // );
+        // console.log(
+        // 	`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
+        // );
+        const symbol = "btcusdt";
+        this.ws.onopen = () => {
+            console.log("바이낸스 웹소켓에 연결되었습니다.");
+            // 심볼의 티커 데이터를 구독
+            const subscribeMessage = JSON.stringify({
+                method: "SUBSCRIBE",
+                params: [`${symbol}@ticker`],
+                id: 1,
+            });
+            this.ws.send(subscribeMessage);
+        };
         this.ws.onmessage = (msg) => {
+            console.log(msg);
             const json = JSON.parse(msg.data);
             p.innerText = json.p;
         };
--- script/Utils/BinanceSocket.ts
+++ script/Utils/BinanceSocket.ts
@@ -1,5 +1,5 @@
 export class BinanceSocket {
-	ws = new WebSocket(`wss://stream.binance.com:9443/ws/btcusdt@trade`);
+	ws: WebSocket = new WebSocket(`wss://stream.binance.com:9443/ws`);
 	ticker: string;
 	constructor(ticker: string) {
 		this.ticker = ticker;
@@ -7,18 +7,33 @@ export class BinanceSocket {
 			`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
 		);
 		this.connect();
+		// this.send();
 	}
 
 	connect() {
 		const p = document.getElementById("ticker-price");
-		this.ws.close();
-		this.ws = new WebSocket(
-			`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
-		);
-		console.log(
-			`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
-		);
+
+		// this.ws.close();
+		// this.ws = new WebSocket(
+		// 	`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
+		// );
+		// console.log(
+		// 	`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
+		// );
+		const symbol = "btcusdt";
+		this.ws.onopen = () => {
+			console.log("바이낸스 웹소켓에 연결되었습니다.");
+
+			// 심볼의 티커 데이터를 구독
+			const subscribeMessage = JSON.stringify({
+				method: "SUBSCRIBE",
+				params: [`${symbol}@ticker`],
+				id: 1,
+			});
+			this.ws.send(subscribeMessage);
+		};
 		this.ws.onmessage = (msg) => {
+			console.log(msg);
 			const json = JSON.parse(msg.data);
 			p!.innerText = json.p;
 		};
