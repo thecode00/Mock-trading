2c21b7886cfc3f276d018c6724935674cf658590
thecode00 <thecode0087@gmail.com>
2024-03-11 18:35:26 +0900

    Change logic

--- script/Utils/BinanceSocket.ts
+++ script/Utils/BinanceSocket.ts
@@ -1,46 +1,54 @@
 export class BinanceSocket {
 	ws: WebSocket = new WebSocket(`wss://stream.binance.com:9443/ws`);
-	ticker: string;
-	constructor(ticker: string) {
-		this.ticker = ticker;
-		console.log(
-			`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
-		);
-		this.connect();
-		// this.send();
+	p: HTMLParagraphElement;
+	ticker = "btc";
+	constructor() {
+		this.p = document.getElementById(
+			"ticker-price"
+		) as HTMLParagraphElement;
+		this.init();
 	}
 
-	connect() {
-		const p = document.getElementById("ticker-price");
-
-		// this.ws.close();
-		// this.ws = new WebSocket(
-		// 	`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
-		// );
-		// console.log(
-		// 	`wss://stream.binance.com:9443/ws/${this.ticker}usdt@trade`
-		// );
-		const symbol = "btcusdt";
+	init() {
+		const symbol = "btc";
 		this.ws.onopen = () => {
 			console.log("바이낸스 웹소켓에 연결되었습니다.");
-
-			// 심볼의 티커 데이터를 구독
-			const subscribeMessage = JSON.stringify({
-				method: "SUBSCRIBE",
-				params: [`${symbol}@ticker`],
-				id: 1,
-			});
-			this.ws.send(subscribeMessage);
+			this.addTickerStream(symbol);
 		};
 		this.ws.onmessage = (msg) => {
-			console.log(msg);
 			const json = JSON.parse(msg.data);
-			p!.innerText = json.p;
+			console.log(json);
+			if (json.s === `${this.ticker}usdt`.toUpperCase()) {
+				this.p!.innerText = json.p;
+			}
 		};
 	}
 
-	changeTicker(ticker: string) {
+	addTickerStream(ticker: string) {
+		const subscribeMessage = JSON.stringify({
+			method: "SUBSCRIBE",
+			params: [`${ticker}usdt@trade`],
+			id: 2,
+		});
+		this.ws!.send(subscribeMessage);
+		const Message = JSON.stringify({
+			method: "LIST_SUBSCRIPTIONS",
+			id: 2,
+		});
+		this.ws.send(Message);
+	}
+
+	deleteTickerStream(ticker: string) {
+		const unsubscribeMessage = JSON.stringify({
+			method: "UNSUBSCRIBE",
+			params: [`${ticker}usdt@trade`],
+			id: 2,
+		});
+		this.ws!.send(unsubscribeMessage);
+	}
+
+	changeOrderTicker(ticker: string) {
 		this.ticker = ticker;
-		this.connect();
+		this.addTickerStream(ticker);
 	}
 }
