de66d4ba43a2d424accb37a4befa566b9d8ec146
thecode00 <thecode0087@gmail.com>
2024-03-14 23:41:55 +0900

    Add comment

--- script/PriceStorage.js
+++ script/PriceStorage.js
@@ -4,6 +4,11 @@ export class PriceStorage {
         this.observers = {};
         this.tickerPrices = {};
     }
+    /**
+     * 저장소에 코인의 가격을 저장하는 함수
+     * @param ticker 가격을 저장할 코인의 티커
+     * @param price 코인 가격
+     */
     setPrice(ticker, price) {
         this.tickerPrices[ticker] = price;
         this.notify(ticker);
@@ -11,13 +16,17 @@ export class PriceStorage {
     getPrice(ticker) {
         return this.tickerPrices[ticker];
     }
+    /**
+     * 코인의 가격이 변경되면 해당 코인의 가격을 구독하고있는 모든 OrderItem에게 알려주는 함수
+     * @param ticker   가격이 변경된 코인의 티커
+     */
     notify(ticker) {
         if (!(ticker in this.observers)) {
             this.observers[ticker] = [];
         }
         this.observers[ticker].map((item) => {
             item.curPrice = this.tickerPrices[item.ticker];
-            item.render(document.body);
+            item.render();
         });
     }
     subscribe(ticker, item) {
@@ -25,6 +34,5 @@ export class PriceStorage {
             this.observers[ticker] = [];
         }
         this.observers[ticker].push(item);
-        console.log(this.observers);
     }
 }
--- script/PriceStorage.ts
+++ script/PriceStorage.ts
@@ -11,8 +11,14 @@ interface tickerPricesInterface {
 // TODO: Add object type
 export class PriceStorage {
 	observers: observerInterface = {};
+
 	tickerPrices: tickerPricesInterface = {};
 
+	/**
+	 * 저장소에 코인의 가격을 저장하는 함수
+	 * @param ticker 가격을 저장할 코인의 티커
+	 * @param price 코인 가격
+	 */
 	setPrice(ticker: string, price: number) {
 		this.tickerPrices[ticker] = price;
 		this.notify(ticker);
@@ -22,13 +28,17 @@ export class PriceStorage {
 		return this.tickerPrices[ticker];
 	}
 
+	/**
+	 * 코인의 가격이 변경되면 해당 코인의 가격을 구독하고있는 모든 OrderItem에게 알려주는 함수
+	 * @param ticker   가격이 변경된 코인의 티커
+	 */
 	notify(ticker: string) {
 		if (!(ticker in this.observers)) {
 			this.observers[ticker] = [];
 		}
 		this.observers[ticker].map((item) => {
 			item.curPrice = this.tickerPrices[item.ticker];
-			item.render(document.body);
+			item.render();
 		});
 	}
 
@@ -37,6 +47,5 @@ export class PriceStorage {
 			this.observers[ticker] = [];
 		}
 		this.observers[ticker].push(item);
-		console.log(this.observers);
 	}
 }
