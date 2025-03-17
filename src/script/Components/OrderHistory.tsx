4d662882233d075267ba8bf7de83e74b48c5d0a2
thecode00 <thecode0087@gmail.com>
2025-03-17 13:41:36 +0900

    Add order history

--- src/script/Components/OrderHistory.tsx
+++ src/script/Components/OrderHistory.tsx
@@ -1,5 +1,19 @@
+import { List } from "@mui/material";
+import { useOrderStore } from "../store/orderStore";
+import { OrderItem } from "./OrderItem";
+
 function OrderHistory() {
-  return <div>OrderHistory</div>;
+  const { orderHistory } = useOrderStore();
+
+  return (
+    <>
+      <List>
+        {orderHistory.map((order) => (
+          <OrderItem key={order.id} data={order} />
+        ))}
+      </List>
+    </>
+  );
 }
 
 export default OrderHistory;
--- src/script/store/orderStore.ts
+++ src/script/store/orderStore.ts
@@ -5,17 +5,25 @@ import { OrderData } from "../../types/order";
 interface OrderStore {
   openPositions: OrderData[];
   orders: OrderData[];
+  orderHistory: OrderData[];
   addOrder: (order: OrderData) => void;
+  addOrderHistory: (order: OrderData) => void;
   checkOrderOpen: () => void;
 }
 
 export const useOrderStore = create<OrderStore>((set, get) => ({
   openPositions: [],
   orders: [],
+  orderHistory: [],
 
   addOrder(order: OrderData) {
     set({ orders: [...get().orders, order] });
+    get().addOrderHistory(order);
     console.log(get().openPositions, get().orders);
+  },
+
+  addOrderHistory(order: OrderData) {
+    set({ orderHistory: [...get().orderHistory, order] });
   },
 
   // order 중에 체결되는 order를 찾는 함수
@@ -32,10 +40,13 @@ export const useOrderStore = create<OrderStore>((set, get) => ({
           (item.type === "long" && item.price >= price) ||
           (item.type === "short" && item.price <= price)
         ) {
-          // 배열에 추가하는 방식으로 수정
+          // 현재 가격보다 높게 사거나 낮게 포지션이 열릴경우 현재가격으로 수정
+          item.price = price;
           set({ openPositions: [...get().openPositions, item] });
+          get().addOrderHistory(item);
           return false;
         }
+
         return true;
       }),
     });
